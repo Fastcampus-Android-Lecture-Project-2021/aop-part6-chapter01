72ece4b28e659fb7d9a82a816330aa068da67fa9
이기정 <jimmy@rainist.com>
2021-02-21 23:31:28 +0900

    00. 프로젝트 셋업하기

---
+++ app/src/main/java/aop/fastcampus/part6/chapter01/MainActivity.kt
@@ -0,0 +1,11 @@
+package aop.fastcampus.part6.chapter01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
